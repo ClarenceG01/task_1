f95ed5ed2c09eedca3b01098b49aeb336b0b045d
unknown <gatama001@gmail.com>
2025-01-31 22:56:29 +0300

    hng 12

--- script.js
+++ script.js
@@ -1,27 +1,9 @@
-function getDay() {
-  const day = new Date().getDate();
-  const days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let currentDay = days[day];
-  console.log(currentDay);
-  return currentDay;
-}
+let currentTime=document.getElementById('currentTimeUTC');
 function getUTCTime() {
   const time = new Date().toUTCString().split(" ")[4];
   const gmt = new Date().toUTCString().split(" ")[5];
+  console.log(time + ' '+gmt)
   return time + " " + gmt;
 }
-getDay();
-const utc_time = document.getElementById("currentTimeUTC");
-const day = document.getElementById("currentDay");
-day.innerHTML = getDay();
-setInterval(() => {
-  utc_time.innerHTML = getUTCTime();
-}, 1000);
+currentTime.innerHTML=getUTCTime();
+
